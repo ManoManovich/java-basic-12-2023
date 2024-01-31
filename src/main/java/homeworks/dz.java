830df20f293bd249989c3546068a5ca324ed962f
Vitaliy Manokhin <manohin.wetal@gmail.com>
2024-01-31 15:19:00 +0300

    готовое дз

--- src/main/java/homeworks/dz.java
+++ src/main/java/homeworks/dz.java
@@ -1,10 +1,12 @@
 package homeworks;
 
 public class dz {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         greetings();
         checkSign(-100, 3, 5);
         selectColor();
+        Numbers();
+        addOrSubtractAndPrint(5, 20, true);
     }
 
     public static void greetings() {
@@ -17,12 +19,14 @@ public class dz {
     public static void checkSign(int a, int b, int c) {
         int sum = a + b + c;
         if (sum >= 0) {
-            System.out.println ("сумма положительная");
+            System.out.println("сумма положительная");
         } else {
-            System.out.println ("число отрицательное");}
+            System.out.println("число отрицательное");
+        }
 
     }
-    public static void selectColor () {
+
+    public static void selectColor() {
         int data = 21;
         if (data <= 10) {
             System.out.println("красный");
@@ -33,5 +37,23 @@ public class dz {
         if (data > 20) {
             System.out.println("зеленый");
         }
+    }
+
+    public static void Numbers() {
+        int a = 1;
+        int b = 10;
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+    public static void addOrSubtractAndPrint (int delta, int initValue, boolean increment) {
+        if (increment) {
+            System.out.println(delta + initValue);
+        }else {
+            System.out.println(delta - initValue);
+        }
+
     }
 }
---
+++ src/main/java/homeworks5/dz.java
@@ -0,0 +1,9 @@
+package homeworks5;
+
+public class dz {
+    public static void main(String[] args) {
+        for (int i = 20; i <= 40; i+=5) {
+            System.out.println(i);
+        }
+    }
+}
